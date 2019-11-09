f7f5fb96a7307571a3b043430d74e3dc5edc4755
Vinicius <vinicius_dogma@hotmail.com>
2019-11-09 14:35:33 -0300

    Incluido entrance e exit na query

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ const typeDefs = gql`
     password: String!
     role: RoleEnum
     registertimes: [RegisterTime]
+    entrance: String
+    exit: String
   }
 
   type RegisterTime {
@@ -67,6 +69,8 @@ const typeDefs = gql`
     email: String!
     password: String!
     role: RoleEnum!
+    entrance: String
+    exit: String
   }
 
   input UpdateUserInput {
@@ -162,6 +166,35 @@ const resolver = {
   Subscription: {
     onCreatedRegister: {
         subscribe: () => pubSub.asyncIterator('createdRegister')
+    }
+  },
+  User: {
+    entrance(parent, body, context, info) {
+      if (!parent.registertimes.length) {
+        return ''
+      }
+      const registers = parent.registertimes.length
+      if (registers === 1) {
+        return parent.registertimes[0].time_registered
+      } else {
+        if (registers % 2) {
+          return parent.registertimes[registers-1].time_registered
+        } else {
+          return parent.registertimes[registers-2].time_registered
+        }
+      }
+    },
+    exit(parent, body, context, info) {
+      const registers = parent.registertimes.length
+      if (registers <= 1) {
+        return ''
+      } else {
+        if (registers % 2) {
+          return parent.registertimes[registers-2].time_registered
+        } else {
+          return parent.registertimes[registers-1].time_registered
+        }
+      }
     }
   }
 }
